d81dba46fb18c8a47c75065993c78c4356965bdf
Jayden <wangkh19930128@gmail.com>
2021-11-17 22:25:41 +1100

    load ingredients, use check_date to compare use-by and best-before with current date (<=) determine ingredient candidates, assign a weight number for best-before 1/100

--- main/main.py
+++ main/main.py
@@ -1,10 +1,9 @@
-
+import json
 from datetime import datetime
 
 
 class PickLunch(object):
     def __init__(self):
-        import json
         with open("../data/recipes.json") as f:
             self.recipes = json.loads(f.read())
         self.new_recipes = self.__convert_recipe()
@@ -16,22 +15,43 @@ class PickLunch(object):
 
         return new_recipes
 
-    def check_useby(self):
+    def check_date(self, datein):
         """
         check uer-by date against current date
         :return: True/False
         """
-        return None
+        today = datetime.today()
+        datein_t = datetime.strptime(datein, '%Y-%m-%d')
+        if today <= datein_t:
+            return True
+        else:
+            return False
 
-    def get_receipe(self):
+    def load_ingredients(self, json_file):
+        with open(json_file) as f:
+            ingredients = json.loads(f.read())
+        # Handle empty file here
 
-        return None
+        return ingredients.get('ingredients')
 
-    def match_receipe(self):
+    def get_ingredients(self, ingredients):
+        in_ingres = []
+        for ing in ingredients:
+            if self.check_date(ing.get('use-by')):
+                if self.check_date(ing.get('best-before')):
+                    in_ingres.append({ing.get('title'): 1})
+                else:
+                    in_ingres.append({ing.get('title'): 100})
+            else:
+                continue
+        return in_ingres
 
-        return None
 
 
 if __name__ == '__main__':
     obj = PickLunch()
     print(obj.new_recipes)
+    ing_json = "../data/ingredients.json"
+    ings = obj.get_ingredients(obj.load_ingredients(ing_json))
+    for i in ings:
+        print(i.keys())
